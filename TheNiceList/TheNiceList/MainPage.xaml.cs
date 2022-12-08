1c8890164c3013714c73fde892a08b9941b41fdd
Connor Buchanan <buchanan.connor@icloud.com>
2022-12-07 17:33:30 -0700

    Started Project -added basic testing layout on maui app -created a handler for the button

---
+++ TheNiceList/TheNiceList/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace TheNiceList;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+	{
+		InitializeComponent();
+
+	}
+
+    private void CheckButtonClickedHandler(Object sender, EventArgs e)
+    {
+        output.Text = CalculateNaughtiness();
+    }
+
+	private string CalculateNaughtiness()
+    {
+        return "hi";
+    }
+}
+
