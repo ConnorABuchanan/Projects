fee9b8aaefe17a69ca91cf6303917cc84cda537c
Connor Buchanan <buchanan.connor@icloud.com>
2022-12-14 12:25:57 -0700

    Basic Functionality in place: -added functinality when the user presses the enter key -made it display naughty or nice randomly -added whitelist: will always return Nicest of Them All if the name entered is on the whitelist. -added persistence across a single instance/run of the program: if a name entered gets naughty the first time, they will be naughty every recurring time until the program is closed.
    Made UI a little better:
    -fixed the layout so it is readable
    
    TODO:
    -make a good looking UI
    -possibly make an animated reveal
    -make a way to add to the whitelist

--- TheNiceList/TheNiceList/MainPage.xaml.cs
+++ TheNiceList/TheNiceList/MainPage.xaml.cs
@@ -4,20 +4,66 @@ namespace TheNiceList;
 
 public partial class MainPage : ContentPage
 {
+    private List<String> whitelist;
+    private Dictionary<String, String> theNiceList;
+
     public MainPage()
 	{
 		InitializeComponent();
-
+        whitelist = new List<String>();
+        theNiceList = new Dictionary<String, String>();
+        fillWhitelist();
 	}
 
     private void CheckButtonClickedHandler(Object sender, EventArgs e)
     {
-        output.Text = CalculateNaughtiness();
+        output.Text = CalculateNaughtiness()+"!";
     }
 
 	private string CalculateNaughtiness()
     {
-        return "hi";
+        string input="";
+        Random rng= new Random();
+
+        if (name.Text != null)
+        {
+            input = name.Text.ToLower();
+            if (whitelist.Contains(input))
+            {
+                return "Nicest of Them All";
+            }
+        }
+
+        string status;
+
+        if (rng.NextDouble() < .5)
+        {
+            status= "Nice";
+        }
+        else
+        {
+            status= "Naughty";
+        }
+
+        if (name.Text != null && !theNiceList.ContainsKey(input) && input!="")
+        {
+            theNiceList.Add(input, status);
+        }
+
+        if(theNiceList.ContainsKey(input))
+        {
+            return theNiceList[input];
+        }
+
+        return status;
+    }
+
+    private void fillWhitelist()
+    {
+        whitelist.Add("papa");
+        whitelist.Add("rich");
+        whitelist.Add("papa rich");
+        whitelist.Add("richard");
     }
 }
 
